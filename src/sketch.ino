cbd0728ba59030281c669402bb44184529b985a7
SOrtiz <sortizurbai@gmail.com>
2025-02-19 20:31:39 -0500

    Lectura RFID (propuesta)

--- src/sketch.ino
+++ src/sketch.ino
@@ -7,26 +7,166 @@
 #include <WiFi.h>
 #include <NTPClient.h>
 #include <HTTPClient.h>
+#include <MFRC522.h>
+#include <SPI.h>
 
+// Pines RFID
+#define SS_PIN 5
+#define RST_PIN 0
 
-#define BUZZER_PIN   14 
-#define GREEN_LED    26 
-#define RED_LED      25 
-#define RELAY_PIN    27 
-#define SERVO_PIN    13
+#define BUZZER_PIN 14
+#define GREEN_LED 26
+#define RED_LED 25
+#define RELAY_PIN 27
+#define SERVO_PIN 13
 
-Servo servo; 
+Servo servo;
+MFRC522 rfid(SS_PIN, RST_PIN); // Instancia de la clase MFRC522
+MFRC522::MIFARE_Key key;
+// Array para guardar el uid de la tarjeta
+byte nuidPICC[4];
 
-#define AWS_IOT_PUBLISH_TOPIC   "molinete/pub"
-#define AWS_IOT_PUBLISH_TOPIC2   "molinete/servo/pub"
-#define AWS_IOT_PUBLISH_TOPIC3   "molinete/rfid/pub"
-#define AWS_IOT_PUBLISH_TOPIC4   "molinete/infrarojo/pub"
+#define AWS_IOT_PUBLISH_TOPIC "molinete/pub"
+#define AWS_IOT_PUBLISH_TOPIC2 "molinete/servo/pub"
+#define AWS_IOT_PUBLISH_TOPIC3 "molinete/rfid/pub"
+#define AWS_IOT_PUBLISH_TOPIC4 "molinete/infrarojo/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "molinete/sub"
 
 WiFiClientSecure net;
 PubSubClient client(net);
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "id.pool.ntp.org");
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Hola");
+  connectAWS();
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+
+  digitalWrite(BUZZER_PIN, LOW);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(RELAY_PIN, LOW);
+
+  servo.attach(SERVO_PIN);
+  servo.write(0);
+  // Inicializa el lector RFID
+  SPI.begin();
+  rfid.PCD_Init();
+  // Asigna la variable de la llave con un valor por defecto
+  for (byte i = 0; i < 6; i++)
+  {
+    key.keyByte[i] = 0xFF;
+  }
+
+  timeClient.begin();
+  timeClient.setTimeOffset(0);
+}
+
+void loop()
+{
+  client.loop();
+  timeClient.update();
+  //Serial.println("Enter e-KTP ID (format: XX XX XX XX):");
+  // Verifica si hay una tarjeta presente y la lee
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
+  {
+    return;
+  }
+  Serial.println("Card detected:");
+  for (byte i = 0; i < rfid.uid.size; i++)
+  {
+    nuidPICC[i] = rfid.uid.uidByte[i];
+    Serial.print(rfid.uid.uidByte[i], HEX);
+  }
+  Serial.println();
+  int cardID = validateCard(nuidPICC, rfid.uid.size);
+  if (cardID > -1)
+  {
+    accessGranted();
+  }
+  else
+  {
+    accessDenied();
+  }
+  publishRFIDData(cardID, access, 1);
+  rfid.PICC_HaltA();
+  
+}
+
+int validateCard(byte uuid[], byte size)
+{
+  HTTPClient http;
+  http.begin(API_URL);
+  http.addHeader("Content-Type", "application/json");
+
+  // Convertir el UUID a un string
+  String uuidStr = "";
+  for (byte i = 0; i < size; i++)
+  {
+    if (uuid[i] < 0x10)
+    {
+      uuidStr += "0";
+    }else{
+      uuidStr += " ";
+    }
+    uuidStr += String(uuid[i], HEX);
+  }
+
+  // Prepare JSON payload
+  String payload = "{\"id\":\"" + uuidStr + "\"}";
+
+  Serial.println("Validating card: " + uuidStr);
+  int httpResponseCode = http.POST(payload);
+
+  if (httpResponseCode > 0)
+  {
+    String response = http.getString();
+    Serial.println("Response: " + response);
+    http.end();
+
+    // Parse the outer JSON response (statusCode and body)
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, response);
+
+    if (!error && doc.containsKey("body"))
+    {
+      // Extract the 'body' JSON string
+      String body = doc["body"].as<String>();
+
+      // Parse the 'body' as a separate JSON object
+      StaticJsonDocument<200> bodyDoc;
+      DeserializationError bodyError = deserializeJson(bodyDoc, body);
+
+      if (!bodyError && bodyDoc.containsKey("card_id"))
+      {
+        // Ensure the card_id is returned as an integer
+        int cardID = bodyDoc["card_id"].as<int>();
+        return cardID; // Return the card ID from the response
+      }
+      else
+      {
+        Serial.println("Invalid body response format");
+        return -1; // Return -1 if card_id is not found in body
+      }
+    }
+    else
+    {
+      Serial.println("Invalid outer response format");
+      return -1; // Return -1 if 'body' is not found
+    }
+  }
+  else
+  {
+    Serial.println("HTTP Request Failed");
+    http.end();
+    return -1; // Return -1 if HTTP request failed
+  }
+}
 
 void connectAWS()
 {
@@ -50,7 +190,7 @@ void connectAWS()
   client.setServer(AWS_IOT_ENDPOINT, 8883);
 
   // Create a message handler
-  //client.setCallback(messageHandler);
+  // client.setCallback(messageHandler);
 
   Serial.println("Connecting to AWS IoT");
 
@@ -72,9 +212,9 @@ void connectAWS()
 
 void publishRFIDData(int cardID, bool access, int puerta)
 {
-  
+
   StaticJsonDocument<200> doc;
-  doc["hora"] = timeClient.getEpochTime() + (7 * 3600);  
+  doc["hora"] = timeClient.getEpochTime() + (7 * 3600);
   doc["ingreso"] = access;
   doc["id"] = cardID;
   doc["puerta"] = puerta;
@@ -83,123 +223,28 @@ void publishRFIDData(int cardID, bool access, int puerta)
   serializeJson(doc, jsonBuffer); // print to client
 
   client.publish(AWS_IOT_PUBLISH_TOPIC3, jsonBuffer);
-  
-}
-
-
-void setup() {
-  Serial.begin(115200); 
-  Serial.println("Hola");
-  connectAWS();
-  pinMode(BUZZER_PIN, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(RED_LED, OUTPUT);
-  pinMode(RELAY_PIN, OUTPUT);
-
-  digitalWrite(BUZZER_PIN, LOW);
-  digitalWrite(GREEN_LED, LOW);
-  digitalWrite(RED_LED, LOW);
-  digitalWrite(RELAY_PIN, LOW);
-
-  servo.attach(SERVO_PIN); 
-  servo.write(0);
-
-  timeClient.begin();
-  timeClient.setTimeOffset(0);
-}
-
-void loop() {
-  if (Serial.available()) {
-    Serial.println("Enter e-KTP ID (format: XX XX XX XX):");
-    String input = Serial.readStringUntil('\n');
-    input.trim(); 
-    Serial.println("CardID: " + input);
-    client.loop();
-    timeClient.update();
-    
-    int cardID = validateCard(input);
-    if (cardID>-1) { 
-      accessGranted();
-        
-    }else {
-      accessDenied();
-    }
-    publishRFIDData(cardID, access, 1);
-   
-   sleep(50);
-  
-  }
- 
-
-}
-
-int validateCard(String cardID) {
-    HTTPClient http;
-    http.begin(API_URL);
-    http.addHeader("Content-Type", "application/json");
-
-    // Prepare JSON payload
-    String payload = "{\"id\":\"" + cardID + "\"}";
-
-    Serial.println("Validating card: " + cardID);
-    int httpResponseCode = http.POST(payload);
-
-    if (httpResponseCode > 0) {
-        String response = http.getString();
-        Serial.println("Response: " + response);
-        http.end();
-
-        // Parse the outer JSON response (statusCode and body)
-        StaticJsonDocument<200> doc;
-        DeserializationError error = deserializeJson(doc, response);
-
-        if (!error && doc.containsKey("body")) {
-            // Extract the 'body' JSON string
-            String body = doc["body"].as<String>();
-
-            // Parse the 'body' as a separate JSON object
-            StaticJsonDocument<200> bodyDoc;
-            DeserializationError bodyError = deserializeJson(bodyDoc, body);
-
-            if (!bodyError && bodyDoc.containsKey("card_id")) {
-                // Ensure the card_id is returned as an integer
-                int cardID = bodyDoc["card_id"].as<int>();
-                return cardID;  // Return the card ID from the response
-            } else {
-                Serial.println("Invalid body response format");
-                return -1;  // Return -1 if card_id is not found in body
-            }
-        } else {
-            Serial.println("Invalid outer response format");
-            return -1;  // Return -1 if 'body' is not found
-        }
-    } else {
-        Serial.println("HTTP Request Failed");
-        http.end();
-        return -1;  // Return -1 if HTTP request failed
-    }
 }
 
 
 
-
-void accessGranted() {
+void accessGranted()
+{
   Serial.println("Access granted!");
   digitalWrite(BUZZER_PIN, HIGH);
   digitalWrite(GREEN_LED, HIGH);
   digitalWrite(RELAY_PIN, HIGH);
-  
-  
-  servo.write(90); 
-  delay(5000); 
-  
-  servo.write(0); 
+
+  servo.write(90);
+  delay(5000);
+
+  servo.write(0);
   digitalWrite(BUZZER_PIN, LOW);
   digitalWrite(GREEN_LED, LOW);
   digitalWrite(RELAY_PIN, LOW);
 }
 
-void accessDenied() {
+void accessDenied()
+{
   Serial.println("Access denied!");
   digitalWrite(BUZZER_PIN, HIGH);
   digitalWrite(RED_LED, HIGH);
