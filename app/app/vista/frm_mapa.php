e0841bf80d750bb3bb6e09204e7c6349c59556fb
DiegoMomo <94811104+DiegoMomo@users.noreply.github.com>
2025-03-01 19:56:55 -0500

    Fix frm_mapa.php

--- app/app/vista/frm_mapa.php
+++ app/app/vista/frm_mapa.php
@@ -156,3 +156,185 @@ foreach($info  as $reg  ){}
         </div>
 
      </div>
+
+    
+  
+<!-- Zonas flotantes
+<button class="toggle-button" id="button1" onclick="toggleLayer(1)">1</button>
+-->    
+  <div class="navegador">
+  <button class="toggle-button active" id="button1" onclick="toggleButton('button1'), toggleLayer(1)">1</button>
+    <button class="toggle-button" id="button2" onclick="toggleButton('button2'), toggleLayer(2)">2</button>
+    <button class="toggle-button" id="button3" onclick="toggleButton('button3'), toggleLayer(3)">3</button>
+
+
+    
+    </div>
+        
+    
+
+    <div id="ventanaEmergenteHorario" style="display: none;">
+    <div class="ventana_horarios">
+        <div class="cabecera">
+            <h2 class="h2_horarios">HOJA DE HORARIOS</h2>
+            <span class="cerrar" id="cerrarVentanaHorarioBtn">&times;</span>
+     </div>
+        
+        <table class="tabla_horarios">
+            <center>
+            <tr>
+                <td>Código</td>
+                <td>Nombre</td>
+                <td>Grupo</td>
+                <td>Entrada</td>
+                <td>Salida</td>
+                <td>Modo</td>
+                <td>Día</td>
+                <td>Profesor</td>
+                <td>Salón</td>
+            </tr>
+            <tr id="fila_horarios">
+                <td>202W1003T</td>
+                <td>PRÁCTICA PRE PROFESIONAL</td>
+                <td>1</td>
+                <td>14:00</td>
+                <td>16:00</td>
+                <td>TEO</td>
+                <td>Sábado</td>
+                <td>MURAKAMI DE LA CRUZ, SUMIKO ELIZABETH </td>
+                <td>105</td>
+            </tr>
+            <tr id="fila_horarios">
+                <td>202W0701T</td>
+                <td>ARQUITECTURA DE SOFTWARE</td>
+                <td>1</td>
+                <td>8:00</td>
+                <td>12:00</td>
+                <td>TEO</td>
+                <td>Sábado</td>
+                <td>MENENDEZ MUERAS, ROSA </td>
+                <td>102</td>
+            </tr>
+            <tr id="fila_horarios">
+                <td>202W0705</td>
+                <td>INTELIGENCIA ARTIFICIAL</td>
+                <td>1</td>
+                <td>14:00</td>
+                <td>18:00</td>
+                <td>TEO</td>
+                <td>Lúnes</td>
+                <td>GAMARRA MORENO, JUAN</td>
+                <td>104</td>
+            </tr>
+            <tr id="fila_horarios">
+                <td>202W0804</td>
+                <td>MINERÍA DE DATOS</td>
+                <td>2</td>
+                <td>16:00</td>
+                <td>22:00</td>
+                <td>TEO</td>
+                <td>Martes</td>
+                <td>CALDERON VILCA, HUGO DAVID</td>
+                <td>106</td>
+            </tr>
+            <tr id="fila_horarios">
+                <td>202W0905</td>
+                <td>GESTIÓN RIESGO DEL SW</td>
+                <td>1</td>
+                <td>16:00</td>
+                <td>22:00</td>
+                <td>TEO</td>
+                <td>Viernes</td>
+                <td>MACHADO VICENTE, JOEL FERNANDO</td>
+                <td>209</td>
+            </tr>
+        <!-- Agrega más filas según sea necesario -->
+        </table>
+    </div>
+    </div>
+
+
+<!-- Contenido salones--> 
+    <!-- Salón 105-->
+    <div id="ventanaEmergenteSalon" class="ventana">
+        <div class="contenido_ventana_salon">
+            <span class="cerrar" id="cerrarSalonBtn">&times;</span>
+            <img src="../../public/img/Salon1.png" alt="Imagen del salón" style="max-width: 100%; max-height: 80vh;">
+            <div class="info_salon">
+
+                <div id='mostrar_datos'></div>
+
+            </div>
+        </div>
+    </div>
+
+      
+     <!--Vista de rutas-->
+    <div id="ruta" style="display: none;">
+        <div id=central>
+            <button type="button"></button>
+            <img src="" alt="imagen RUTA">
+            <button type="button"></button>
+        </div>
+        <div id=aceptar>
+            <button type="button" onclick="mostrarMapa()">Aceptar</button>
+        </div>
+        
+    </div>
+
+
+    <!--AGREGAR BOOTSTRAP (JSCRIPT)-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
+    <script src="../../public/js/mapa.js"></script>
+
+    <script>
+// Variables para botón 105
+var botonAula105 = document.getElementById('mostrarInfo');
+var ventanaEmergenteSalon = document.getElementById('ventanaEmergenteSalon');
+var cerrarSalonBtn = document.getElementById('cerrarSalonBtn');
+// Variables para botón 106
+var botonAula106 = document.getElementById('mostrarInfo2');
+var ventanaEmergenteSalon2 = document.getElementById('ventanaEmergenteSalon2');
+var cerrarSalonBtn2 = document.getElementById('cerrarSalonBtn2');
+// Variables para Vista Horario
+var botonMostrarHorario = document.getElementById('cambia_MapaHorario');
+var ventanaEmergenteHorario = document.getElementById('ventanaEmergenteHorario');
+var cerrarVentanaHorarioBtn = document.getElementById('cerrarVentanaHorarioBtn');
+// Función para ocultar ventana y mostrar botón
+function ocultarVentanaYMostrarBoton(ventana, boton) {
+    ventana.style.display = 'none';
+    //boton.style.display = 'block';
+}
+
+// Agrega un evento de clic al botón de cierre de la ventana del salón 105
+cerrarSalonBtn.addEventListener('click', function() {
+    ocultarVentanaYMostrarBoton(ventanaEmergenteSalon, botonAula105);
+    ocultarVentanaYMostrarBoton(ventanaEmergenteSalon, botonMostrarHorario);
+    //botonAula105.style.borderWidth = '2px';
+    //botonAula105.style.borderColor = 'black';
+    //botonAula105.style.borderStyle = 'solid';
+});
+
+
+// Agrega un evento de clic al botón "Vista horario"
+botonMostrarHorario.addEventListener('click', function() {
+    ventanaEmergenteHorario.style.display = 'block';
+    //botonMostrarHorario.style.display = 'none';
+    //botonAula105.style.display = 'none';
+    //botonAula106.style.display = 'none';
+});
+// Agrega un evento de clic al botón de cierre de la ventana de horario
+cerrarVentanaHorarioBtn.addEventListener('click', function() {
+    ventanaEmergenteHorario.style.display = 'none';
+    //botonMostrarHorario.style.display = 'block';
+    //botonAula105.style.display = 'block';
+    //botonAula106.style.display = 'block';
+});
+</script>
+</body>
+<script type="text/javascript" src="../../public/js/mapa_config.js"></script>
+
+</html>
+<!--Prueba!>
